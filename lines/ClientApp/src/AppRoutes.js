dbeca86b18bef64b0d719479a18b12aa25c272cc
thegradle <adamchuk.v.1@gmail.com>
2023-08-01 21:42:10 +0300

    Add route

--- lines/ClientApp/src/AppRoutes.js
+++ lines/ClientApp/src/AppRoutes.js
@@ -1,10 +1,15 @@
 import Home from "./components/Home";
+import PostPage from "./components/PostPage";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  }
+    {
+        index: true,
+        element: <Home />
+    },
+    {
+        path: "/post",
+        element: <PostPage />
+    }
 ];
 
 export default AppRoutes;
