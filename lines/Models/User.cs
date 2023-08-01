65e6dda1c83ca80a02f4138957ecb08b76eacde5
thegradle <adamchuk.v.1@gmail.com>
2023-08-01 20:49:45 +0300

    Fix bug

--- lines/Models/User.cs
+++ lines/Models/User.cs
@@ -11,7 +11,6 @@
         public string? Avatar { get; set; }
         public IEnumerable<Post> Posts { get; set; }
         public IEnumerable<Repost> Reposts { get; set; }
-        public IEnumerable<Reply> Replies { get; set; }
         public IEnumerable<Follower> Followers { get; set; }
         public IEnumerable<Follower> Following { get; set; }
         public IEnumerable<Like> Likes { get; set; }
