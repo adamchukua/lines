999a0ae7b6419e30d75f13c9123ecaadb91dd60b
thegradle <adamchuk.v.1@gmail.com>
2023-08-02 13:16:20 +0300

    Add route to profile page

--- lines/ClientApp/src/AppRoutes.js
+++ lines/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import NotificationsPage from "./components/NotificationsPage";
 import PostPage from "./components/PostPage";
+import ProfilePage from "./components/ProfilePage";
 import SettingsPage from "./components/SettingsPage";
 
 const AppRoutes = [
@@ -19,6 +20,10 @@ const AppRoutes = [
     {
         path: "/settings",
         element: <SettingsPage />
+    },
+    {
+        path: "/profile",
+        element: <ProfilePage />
     }
 ];
 
