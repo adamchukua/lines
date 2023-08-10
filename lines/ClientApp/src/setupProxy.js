d8400ce137d9b9c50b505114416b0338ae5e280b
thegradle <adamchuk.v.1@gmail.com>
2023-08-10 12:49:06 +0300

    Setup proxy: add swagger and posts api

--- lines/ClientApp/src/setupProxy.js
+++ lines/ClientApp/src/setupProxy.js
@@ -4,8 +4,9 @@ const { env } = require('process');
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:47128';
 
-const context =  [
-  "/weatherforecast",
+const context = [
+    "/swagger",
+    "/api/Posts",
 ];
 
 module.exports = function(app) {
