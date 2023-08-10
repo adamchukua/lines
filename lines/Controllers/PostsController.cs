72a2bb7fa20a88e7eda2130b3f71f850f8b89887
thegradle <adamchuk.v.1@gmail.com>
2023-08-10 11:24:02 +0300

    Add PostsController with method GetAllPosts

---
+++ lines/Controllers/PostsController.cs
@@ -0,0 +1,26 @@
+﻿using Lines.Entities;
+using Lines.Services.PostsService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lines.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly PostsService _postsService;
+
+        public PostsController(PostsService postsService)
+        {
+            _postsService = postsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Post>>> GetAllPosts()
+        {
+            var posts = await _postsService.GetAllPostsAsync();
+            return Ok(posts);
+        }
+    }
+}
