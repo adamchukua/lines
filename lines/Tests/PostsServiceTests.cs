3dca152bcb479ebbb30e230941b9616ce336ace1
thegradle <adamchuk.v.1@gmail.com>
2023-08-10 11:41:11 +0300

    Add test get all posts for Posts service

---
+++ lines/Tests/PostsServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using Lines.Entities;
+using Lines.Infrastructure.Data;
+using Lines.Services.PostsService;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace Lines.Tests
+{
+    [TestFixture]
+    public class PostsServiceTests
+    {
+        private LinesDbContext _dbContext;
+        private PostsService _postsService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<LinesDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _dbContext = new LinesDbContext(options);
+
+            _postsService = new PostsService(_dbContext);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _dbContext.Dispose();
+        }
+
+        [Test]
+        public async Task GetAllPostsAsync_ShouldReturnAllPosts()
+        {
+            var user = new User { Id = 1, UserName = "testuser", Name = "testuser"};
+            _dbContext.Users.Add(user);
+
+            var posts = new List<Post>
+            {
+                new Post { Id = 1, UserId = user.Id, Text = "Test post 1", CreatedAt = DateTime.Now },
+                new Post { Id = 2, UserId = user.Id, Text = "Test post 2", CreatedAt = DateTime.Now }
+            };
+            _dbContext.Posts.AddRange(posts);
+            await _dbContext.SaveChangesAsync();
+
+            var result = await _postsService.GetAllPostsAsync();
+
+            Assert.AreEqual(posts.Count, result.Count);
+            Assert.IsTrue(result.All(p => posts.Any(expected => expected.Id == p.Id)));
+        }
+    }
+}
