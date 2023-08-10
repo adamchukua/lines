ca4b59ed03bab3141079316157ddabeea2acb0c0
thegradle <adamchuk.v.1@gmail.com>
2023-08-10 12:49:48 +0300

    Map posts in PostsList

--- lines/ClientApp/src/features/posts/PostsList.js
+++ lines/ClientApp/src/features/posts/PostsList.js
@@ -1,20 +1,14 @@
 import Post from "../../common/Post";
 import Thread from "../../common/Thread";
 
-export default function PostsList() {
+export default function PostsList({ posts }) {
     return (
-        <div>
-            <div className="box post">
-                <Post />
-            </div>
-
-            <div className="box post">
-                <Thread />
-            </div>
-
-            <div className="box post">
-                <Post />
-            </div>
-        </div>
+        <>
+            {posts && posts.map((post) => (
+                <div className="box post" key={post.id}>
+                    <Post post={post} />
+                </div>    
+            ))}
+        </>
     );
 }
