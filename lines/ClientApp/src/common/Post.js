96fb2fec9cb310e7b76d853281716d6a23856d12
thegradle <adamchuk.v.1@gmail.com>
2023-08-11 13:10:23 +0300

    Display posts with threads

--- lines/ClientApp/src/common/Post.js
+++ lines/ClientApp/src/common/Post.js
@@ -1,67 +1,73 @@
-export default function Post({ isThread }) {
+export default function Post({ isThread, post }) {
+    console.log(post);
+
     return (
-        <article className="media">
-            <div className="media-left">
-                <figure className="image is-64x64">
-                    <img src="https://bulma.io/images/placeholders/128x128.png" className="is-rounded" alt="Image" />
+        <>
+            {post && (
+                <article className="media">
+                    <div className="media-left">
+                        <figure className="image is-64x64">
+                            <img src={post.user.avatar} className="is-rounded" alt="Image" />
 
-                    {isThread && (<div className="thread-line"></div>)}
-                </figure>
-            </div>
-
-            <div className="media-content">
-                <div className="content">
-                    <div className="is-flex is-justify-content-space-between">
-                        <p>
-                            <strong>John Smith</strong> <small>@johnsmith</small>
-                        </p>
-
-                        <p>
-                            <small>31m</small>
-                        </p>
+                            {isThread && (<div className="thread-line"></div>)}
+                        </figure>
                     </div>
 
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                    </p>
-                </div>
+                    <div className="media-content">
+                        <div className="content">
+                            <div className="is-flex is-justify-content-space-between">
+                                <p>
+                                    <strong>{post.user.name}</strong> <small>@{post.user.userName}</small>
+                                </p>
 
-                <nav className="level is-mobile mt-5">
-                    <div className="level-left">
-                        <a className="level-item text-black" aria-label="reply">
-                            <span className="icon is-small">
-                                <i className="fas fa-reply" aria-hidden="true"></i>
-                            </span>
+                                <p>
+                                    <small>31m</small>
+                                </p>
+                            </div>
 
-                            <p className="ml-2">0</p>
-                        </a>
+                            <p>
+                                {post.text}
+                            </p>
+                        </div>
 
-                        <a className="level-item text-black" aria-label="retweet">
-                            <span className="icon is-small">
-                                <i className="fas fa-retweet" aria-hidden="true"></i>
-                            </span>
+                        <nav className="level is-mobile mt-5">
+                            <div className="level-left">
+                                <a className="level-item text-black" aria-label="reply">
+                                    <span className="icon is-small">
+                                        <i className="fas fa-reply" aria-hidden="true"></i>
+                                    </span>
 
-                            <p className="ml-2">0</p>
-                        </a>
+                                    <p className="ml-2">{post.replies.length}</p>
+                                </a>
 
-                        <a className="level-item text-black" aria-label="like">
-                            <span className="icon is-small">
-                                <i className="fas fa-heart" aria-hidden="true"></i>
-                            </span>
+                                <a className="level-item text-black" aria-label="retweet">
+                                    <span className="icon is-small">
+                                        <i className="fas fa-retweet" aria-hidden="true"></i>
+                                    </span>
 
-                            <p className="ml-2">0</p>
-                        </a>
+                                    <p className="ml-2">{post.reposts.length}</p>
+                                </a>
 
-                        <a className="level-item text-black" aria-label="share">
-                            <span className="icon is-small">
-                                <i className="fas fa-share" aria-hidden="true"></i>
-                            </span>
+                                <a className="level-item text-black" aria-label="like">
+                                    <span className="icon is-small">
+                                        <i className="fas fa-heart" aria-hidden="true"></i>
+                                    </span>
 
-                            <p className="ml-2">0</p>
-                        </a>
+                                    <p className="ml-2">{post.likes.length}</p>
+                                </a>
+
+                                <a className="level-item text-black" aria-label="share">
+                                    <span className="icon is-small">
+                                        <i className="fas fa-share" aria-hidden="true"></i>
+                                    </span>
+
+                                    <p className="ml-2">0</p>
+                                </a>
+                            </div>
+                        </nav>
                     </div>
-                </nav>
-            </div>
-        </article>
+                </article>
+            )}
+        </>
     );
 }
--- lines/ClientApp/src/features/posts/PostsList.js
+++ lines/ClientApp/src/features/posts/PostsList.js
@@ -2,12 +2,30 @@ import Post from "../../common/Post";
 import Thread from "../../common/Thread";
 
 export default function PostsList({ posts }) {
+    const findReply = (post) => {
+        return posts.find(p => p.repliedPostId == post.id);
+    }
+
+    const findOriginalPost = (reply) => {
+        return posts.find(p => p.id == reply.repliedPostId);
+    }
+
     return (
         <>
-            {posts && posts.map((post) => (
-                <div className="box post" key={post.id}>
-                    <Post post={post} />
-                </div>    
+            {posts && posts.map((post, index) => (
+                <>
+                    {findReply(post) ? (
+                        <Thread posts={[post, findReply(post)]} />    
+                    ) : (
+                        <>
+                            {findOriginalPost(post) ? (
+                                <Thread posts={[findOriginalPost(post), post]} />
+                            ) : (
+                                <Post post={post} />
+                            )}
+                        </>
+                    )}
+                </>
             ))}
         </>
     );
