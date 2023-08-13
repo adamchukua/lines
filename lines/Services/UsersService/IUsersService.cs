3994760c23ca3ca29114db280cca91fbd01188e4
thegradle <adamchuk.v.1@gmail.com>
2023-08-13 11:10:55 +0300

    Add user service with GetUser()

---
+++ lines/Services/UsersService/IUsersService.cs
@@ -0,0 +1,9 @@
+﻿using Lines.DTOs;
+
+namespace Lines.Services.UsersService
+{
+    public interface IUsersService
+    {
+        Task<UserDetailedInfoDTO> GetUserAsync(string userName);
+    }
+}
---
+++ lines/Services/UsersService/UsersService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Lines.DTOs;
+using Lines.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lines.Services.UsersService
+{
+    public class UsersService : IUsersService
+    {
+        private readonly LinesDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public UsersService(LinesDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDetailedInfoDTO> GetUserAsync(string userName)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            var userDto = _mapper.Map<UserDetailedInfoDTO>(user);
+            return userDto;
+        }
+    }
+}
