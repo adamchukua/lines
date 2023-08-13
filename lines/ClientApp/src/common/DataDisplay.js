c3ca43951578cb563b0cf7b31a9bb5ea7821d238
thegradle <adamchuk.v.1@gmail.com>
2023-08-13 10:12:44 +0300

    Minor fixes

--- lines/ClientApp/src/common/DataDisplay.js
+++ lines/ClientApp/src/common/DataDisplay.js
@@ -1,7 +1,7 @@
 export default function DataDisplay({ status, error, children }) {
     if (status === 'loading') {
         return (
-            <progress class="progress is-medium is-dark" max="100">45%</progress>
+            <progress className="progress is-medium is-dark" max="100">45%</progress>
         );
     } else if (status === 'failed') {
         return <>Error: {error}</>;
--- lines/ClientApp/src/features/posts/PostsList.js
+++ lines/ClientApp/src/features/posts/PostsList.js
@@ -1,22 +1,23 @@
+import React from "react";
 import DataDisplay from "../../common/DataDisplay";
 import Post from "../../common/Post";
 import Thread from "../../common/Thread";
 
 export default function PostsList({ posts }) {
     const findOriginalPost = (reply) => {
-        return posts.posts.find(p => p.id == reply.repliedPostId);
+        return posts.posts.find(p => p.id === reply.repliedPostId);
     }
 
     return (
         <DataDisplay status={posts.status} error={posts.error}>
             {posts.posts.map((post) => (
-                <>
+                <React.Fragment key={post.id}>
                     {findOriginalPost(post) ? (
                         <Thread posts={[findOriginalPost(post), post]} />
                     ) : (
                         <Post post={post} />
                     )}
-                </>
+                </React.Fragment>
             ))}
         </DataDisplay>
     );
