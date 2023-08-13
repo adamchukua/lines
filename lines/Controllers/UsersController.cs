b78b8c8955aa8df67c3799ca467d24dc26dc6305
thegradle <adamchuk.v.1@gmail.com>
2023-08-13 11:11:38 +0300

    Add UsersController with GetUser() and set up proxy

---
+++ lines/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Lines.DTOs;
+using Lines.Services.UsersService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lines.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersService _usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<UserDetailedInfoDTO>> GetUser(string userName)
+        {
+            var user = await _usersService.GetUserAsync(userName);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
