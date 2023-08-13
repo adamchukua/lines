7101bacff4c7524f50317032bbadca798f150f7d
thegradle <adamchuk.v.1@gmail.com>
2023-08-13 09:57:34 +0300

    Delete findReply()

--- lines/ClientApp/src/features/posts/PostsList.js
+++ lines/ClientApp/src/features/posts/PostsList.js
@@ -3,10 +3,6 @@ import Post from "../../common/Post";
 import Thread from "../../common/Thread";
 
 export default function PostsList({ posts }) {
-    const findReply = (post) => {
-        return posts.posts.find(p => p.repliedPostId == post.id);
-    }
-
     const findOriginalPost = (reply) => {
         return posts.posts.find(p => p.id == reply.repliedPostId);
     }
@@ -15,16 +11,10 @@ export default function PostsList({ posts }) {
         <DataDisplay status={posts.status} error={posts.error}>
             {posts.posts.map((post) => (
                 <>
-                    {findReply(post) ? (
-                        <Thread posts={[post, findReply(post)]} />    
+                    {findOriginalPost(post) ? (
+                        <Thread posts={[findOriginalPost(post), post]} />
                     ) : (
-                        <>
-                            {findOriginalPost(post) ? (
-                                <Thread posts={[findOriginalPost(post), post]} />
-                            ) : (
-                                <Post post={post} />
-                            )}
-                        </>
+                        <Post post={post} />
                     )}
                 </>
             ))}
