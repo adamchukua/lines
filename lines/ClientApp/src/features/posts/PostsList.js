5cf2c9849db443cdf3b13d6a1f4cb7cc7623a293
thegradle <adamchuk.v.1@gmail.com>
2023-08-15 13:41:18 +0300

    Delete func findOriginalPost() and use instead post from api

--- lines/ClientApp/src/features/posts/PostsList.js
+++ lines/ClientApp/src/features/posts/PostsList.js
@@ -3,16 +3,12 @@ import Post from "../../common/Post";
 import Thread from "../../common/Thread";
 
 export default function PostsList({ posts }) {
-    const findOriginalPost = (reply) => {
-        return posts.find(p => p.id === reply.repliedPostId);
-    }
-
     return (
         <>
             {posts?.map((post) => (
                 <React.Fragment key={post.id}>
-                    {findOriginalPost(post) ? (
-                        <Thread posts={[findOriginalPost(post), post]} />
+                    {post.parentPost ? (
+                        <Thread posts={[post.parentPost, post]} />
                     ) : (
                         <Post post={post} />
                     )}
