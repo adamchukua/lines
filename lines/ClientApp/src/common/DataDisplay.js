37a3862ec909c42cda7ec5524b1f8f66d71fc177
thegradle <adamchuk.v.1@gmail.com>
2023-08-15 13:19:04 +0300

    Add infinite scrolling component

--- lines/ClientApp/src/common/DataDisplay.js
+++ lines/ClientApp/src/common/DataDisplay.js
@@ -1,5 +1,15 @@
-export default function DataDisplay({ status, error, children }) {
+export default function DataDisplay({ status, error, children, isInfiniteScroll }) {
     if (status === 'loading') {
+        if (isInfiniteScroll) {
+            return (
+                <>
+                    {children}
+
+                    <progress className="progress is-medium is-dark mt-4" max="100">45%</progress>
+                </>
+            );
+        }
+
         return (
             <progress className="progress is-medium is-dark" max="100">45%</progress>
         );
--- lines/ClientApp/src/common/Home.js
+++ lines/ClientApp/src/common/Home.js
@@ -1,20 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPosts } from "../features/posts/postsSlice"
+import { fetchPosts } from "../features/posts/postsSlice";
+import InfiniteScroll from 'react-infinite-scroll-component';
 import PostsList from "../features/posts/PostsList";
 import DataDisplay from './DataDisplay';
 
 export default function Home() {
     const dispatch = useDispatch();
     const posts = useSelector((state) => state.posts);
+    const [page, setPage] = useState(1);
+    const hasMore = posts.posts.length > 0;
 
     useEffect(() => {
-        dispatch(fetchPosts());
-    }, [dispatch]);
+        dispatch(fetchPosts({ pageNumber: page }));
+    }, [dispatch, page]);
+
+    const loadMore = () => {
+        setPage(prevPage => prevPage + 1);
+    };
 
     return (
-        <DataDisplay status={posts.status} error={posts.error}>
-            <PostsList posts={posts.posts} />
+        <DataDisplay status={posts.status} error={posts.error} isInfiniteScroll={true}>
+            <InfiniteScroll
+                dataLength={posts.posts.length}
+                next={loadMore}
+                hasMore={hasMore}
+            >
+                <PostsList posts={posts.posts} />
+            </InfiniteScroll>
         </DataDisplay>
     );
 }
