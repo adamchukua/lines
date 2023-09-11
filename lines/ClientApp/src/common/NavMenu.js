dc0657aa1ae2a501bcc8be11de4c63e4803e4802
thegradle <adamchuk.v.1@gmail.com>
2023-09-11 17:27:51 +0300

    Update NavMenu, add login and logout

--- lines/ClientApp/src/common/NavMenu.js
+++ lines/ClientApp/src/common/NavMenu.js
@@ -1,19 +1,54 @@
 import NewPostModal from "./NewPostModal";
+import userManager from '../app/oidc-config';
+import { useEffect, useState } from "react";
 
 export default function NavMenu() {
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        userManager.getUser().then((user) => {
+            if (user) {
+                setUser(user);
+                console.log(user.profile);
+            }
+        });
+    }, []);
+
+    const handleLogin = () => {
+        userManager.signinRedirect();
+    };
+
+    const handleLogout = () => {
+        userManager.signoutRedirect();
+    };
+
     return (
         <>
             <nav className="pos-fix">
-                <img src="images/logo_big.svg" className="logo" alt="lines" />
+                <a href="/">
+                    <img src="images/logo_big.svg" className="logo" alt="lines" />
+                </a>
 
-                <ul className="menu-list my-4">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/profile">Profile</a></li>
-                    <li><a href="/notifications">Notifications</a></li>
-                    <li><a href="/settings">Settings</a></li>
-                </ul>
+                {!user?.profile ? (
+                    <div className="is-flex is-flex-direction-column mt-5">
+                        To use lines fully pleas log in.
 
-                <NewPostModal />
+                        <button className="btn mt-4" onClick={handleLogin}>Login</button>
+                    </div>
+                ) : (
+                    <nav className="pos-fix">
+                        <ul className="menu-list my-4">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/profile">Profile</a></li>
+                            <li><a href="/notifications">Notifications</a></li>
+                            <li><a href="/settings">Settings</a></li>
+
+                            <button className="btn mt-4" onClick={handleLogout}>Logout</button>
+                        </ul>
+
+                        <NewPostModal />
+                    </nav>
+                )}
             </nav>
         </>
     );
