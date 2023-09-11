d42ad161ccdbb2f4f8ebc6f602f797f6fa45c0a8
thegradle <adamchuk.v.1@gmail.com>
2023-09-11 17:26:33 +0300

    Add mvc set up for IdentityServer

---
+++ IdentityServer/Controllers/AuthController.cs
@@ -0,0 +1,160 @@
+﻿using System.Security.Claims;
+using IdentityServer.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly IIdentityServerInteractionService _interactionService;
+
+        public AuthController(
+            SignInManager<User> signInManager, 
+            UserManager<User> userManager,
+            IIdentityServerInteractionService interactionService)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _interactionService = interactionService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout(string logoutId)
+        {
+            await _signInManager.SignOutAsync();
+
+            var logoutRequest = await _interactionService.GetLogoutContextAsync(logoutId);
+
+            if (string.IsNullOrEmpty(logoutRequest.PostLogoutRedirectUri))
+            {
+                return RedirectToAction("Index", "Home");   
+            }
+
+            return Redirect(logoutRequest.PostLogoutRedirectUri);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            var externalProviders = await _signInManager.GetExternalAuthenticationSchemesAsync();
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl, 
+                ExternalProviders = externalProviders
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            // check if the model is valid 
+
+            var result = await _signInManager.PasswordSignInAsync(
+                loginViewModel.Username, 
+                loginViewModel.Password,
+                false, 
+                false);
+
+            if (result.Succeeded)
+            {
+                return Redirect(loginViewModel.ReturnUrl);
+            }
+            else if (result.IsLockedOut)
+            {
+
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            return View(new RegisterViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerViewModel);
+            }
+
+            var user = new User(registerViewModel.Username, registerViewModel.Name);
+            var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+
+            if (result.Succeeded)
+            { 
+                await _signInManager.SignInAsync(user, false);
+                return Redirect(registerViewModel.ReturnUrl);
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> ExternalLogin(string provider, string returnUrl)
+        {
+            var redirectUri = Url.Action(nameof(ExternalLoginCallback), "Auth", new { returnUrl });
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUri);
+            return Challenge(properties, provider);
+        }
+
+        public async Task<IActionResult> ExternalLoginCallback(string returnUrl)
+        {
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+
+            if (info == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, false);
+
+            if (result.Succeeded)
+            {
+                return Redirect(returnUrl);
+            }
+
+            var name = info.Principal.FindFirst(ClaimTypes.Name).Value;
+            return View("ExternalRegister", new ExternalRegisterViewModel
+            {
+                Name = name,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public async Task<IActionResult> ExternalRegister(ExternalRegisterViewModel externalRegister)
+        {
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+
+            if (info == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var user = new User(externalRegister.Username, externalRegister.Name);
+            var result = await _userManager.CreateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return View(externalRegister);
+            }
+
+            var addLogin = await _userManager.AddLoginAsync(user, info);
+
+            if (!addLogin.Succeeded)
+            {
+                return View(externalRegister);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+
+            return Redirect(externalRegister.ReturnUrl);
+        }
+    }
+}
---
+++ IdentityServer/Models/ExternalRegisterViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityServer.Models
+{
+    public class ExternalRegisterViewModel
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
