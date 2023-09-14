c69b075f9738f5a2ea230970b6d5d49d291e233d
thegradle <adamchuk.v.1@gmail.com>
2023-09-14 10:36:53 +0300

    Call addPost in ReplyInput component

--- lines/ClientApp/src/common/ReplyInput.js
+++ lines/ClientApp/src/common/ReplyInput.js
@@ -1,7 +1,27 @@
-export default function ReplyInput() {
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addPost } from '../features/posts/postsSlice';
+
+export default function ReplyInput({ postId }) {
+    const dispatch = useDispatch();
+    const [replyText, setReplyText] = useState('');
+
+    const handleEnterPress = (e) => {
+        if (e.key === 'Enter' && replyText.trim() !== '') {
+            dispatch(addPost({ repliedPostId: postId, text: replyText }));
+            setReplyText('');
+        }
+    };
+
     return (
         <>
-            <input placeholder="Type your reply" className="reply my-5" />
+            <input
+                placeholder="Type your reply"
+                className="reply my-5"
+                value={replyText}
+                onChange={(e) => setReplyText(e.target.value)}
+                onKeyPress={handleEnterPress}
+            />
         </>
     );
 }
